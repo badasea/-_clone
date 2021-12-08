ccf57c785ed8f3e8ef55482a8842cfad95e5f7cc
jungbada269 <57929751+jungbada269@users.noreply.github.com>
2021-12-09 05:07:30 +0900

    화면 프레그먼트 연결 중

--- Howlstagram/app/src/main/java/com/example/howlstagram/MainActivity.kt
+++ Howlstagram/app/src/main/java/com/example/howlstagram/MainActivity.kt
@@ -1,11 +1,88 @@
 package com.example.howlstagram
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.howlstagram.navigation.AddPhotoActivity
+import com.example.howlstagram.navigation.AlarmFragment
+import com.example.howlstagram.navigation.DetailViewFragment
+import com.example.howlstagram.navigation.UserFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.auth.User
+import java.util.jar.Manifest
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        progress_bar.visibility = View.VISIBLE
+
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
+        bottom_navigation.selectedItemId = R.id.action_home
+
+        ActivityCompat.requestPermissions(this,
+        arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+    }
+    fun setToolbarDefault() {
+        toolbar_title_image.visibility = View.VISIBLE
+        toolbar_btn_back.visibility = View.GONE
+        toolbar_username.visibility = View.GONE
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        setToolbarDefault()
+        when (item.itemId) {
+            R.id.action_home -> {
+                val detailViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, detailViewFragment)
+                    .commit()
+
+                return true
+            }
+            R.id.action_search -> {
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.main_content, gridFragment)
+                    .commit()
+                return true
+            }
+            R.id.action_add_photo -> {
+                if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    startActivity(Intent(this, AddPhotoActivity::class.java))
+                } else {
+                    Toast.makeText(this, "스토리지 읽기 권한이 없습니다.",
+                    Toast.LENGTH_LONG).show()
+                }
+                return true
+            }
+            R.id.action_favorite_alarm -> {
+                val alarmFragment = AlarmFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, alarmFragment)
+                    .commit()
+                return true
+            }
+            R.id.action_account -> {
+                val userFragment = UserFragment()
+                val uid = FirebaseAuth.getInstance().currentUser!!.uid
+                val bundle = Bundle()
+
+                bundle.putString("destinationUid", uid)
+                userFragment.arguments = bundle
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, userFragment)
+                    .commit()
+                return true
+            }
+        }
     }
 }

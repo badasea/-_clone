a3aa48f88a62e9ea335fef9c5007add73e46e2c3
jungbada269 <57929751+jungbada269@users.noreply.github.com>
2021-12-10 02:35:26 +0900

    게시물 등록 기능 구현

--- Howlstagram/app/src/main/java/com/example/howlstagram/MainActivity.kt
+++ Howlstagram/app/src/main/java/com/example/howlstagram/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.howlstagram
 
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -14,22 +16,39 @@ import com.example.howlstagram.navigation.DetailViewFragment
 import com.example.howlstagram.navigation.UserFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.auth.User
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.jar.Manifest
 
+
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    val PICK_PROFILE_FROM_ALBUM = 10
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         progress_bar.visibility = View.VISIBLE
 
+        // Bottom Navigation View
         bottom_navigation.setOnNavigationItemSelectedListener(this)
         bottom_navigation.selectedItemId = R.id.action_home
 
-        ActivityCompat.requestPermissions(this,
-        arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+        // 앨범 접근 권한 요청
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+        //푸시토큰 서버 등록
+        registerPushToken()
     }
+
+    fun registerPushToken(){
+        var pushToken = FirebaseInstanceId.getInstance().token
+        var uid = FirebaseAuth.getInstance().currentUser?.uid
+        var map = mutableMapOf<String,Any>()
+        map["pushtoken"] = pushToken!!
+        FirebaseFirestore.getInstance().collection("pushtokens").document(uid!!).set(map)
+    }
+
     fun setToolbarDefault() {
         toolbar_title_image.visibility = View.VISIBLE
         toolbar_btn_back.visibility = View.GONE
@@ -40,14 +59,15 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         setToolbarDefault()
         when (item.itemId) {
             R.id.action_home -> {
+
                 val detailViewFragment = DetailViewFragment()
                 supportFragmentManager.beginTransaction()
                     .replace(R.id.main_content, detailViewFragment)
                     .commit()
-
                 return true
             }
             R.id.action_search -> {
+                val gridFragment = GridFragment()
                 supportFragmentManager
                     .beginTransaction()
                     .replace(R.id.main_content, gridFragment)
@@ -55,18 +75,17 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
                 return true
             }
             R.id.action_add_photo -> {
-                if (ContextCompat.checkSelfPermission(this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
                     startActivity(Intent(this, AddPhotoActivity::class.java))
                 } else {
-                    Toast.makeText(this, "스토리지 읽기 권한이 없습니다.",
-                    Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, "스토리지 읽기 권한이 없습니다.", Toast.LENGTH_LONG).show()
                 }
                 return true
             }
             R.id.action_favorite_alarm -> {
                 val alarmFragment = AlarmFragment()
-                supportFragmentManager.beginTransaction()
+                supportFragmentManager
+                    .beginTransaction()
                     .replace(R.id.main_content, alarmFragment)
                     .commit()
                 return true
@@ -75,7 +94,6 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
                 val userFragment = UserFragment()
                 val uid = FirebaseAuth.getInstance().currentUser!!.uid
                 val bundle = Bundle()
-
                 bundle.putString("destinationUid", uid)
                 userFragment.arguments = bundle
                 supportFragmentManager.beginTransaction()
@@ -84,5 +102,32 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
                 return true
             }
         }
+        return false
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        // 앨범에서 Profile Image 사진 선택시 호출 되는 부분분
+        if (requestCode == PICK_PROFILE_FROM_ALBUM && resultCode == Activity.RESULT_OK) {
+
+            var imageUri = data?.data
+
+
+            val uid = FirebaseAuth.getInstance().currentUser!!.uid //파일 업로드
+            //사진을 업로드 하는 부분  userProfileImages 폴더에 uid에 파일을 업로드함
+            FirebaseStorage
+                .getInstance()
+                .reference
+                .child("userProfileImages")
+                .child(uid)
+                .putFile(imageUri!!)
+                .addOnCompleteListener { task ->
+                    val url = task.result.downloadUrl.toString()
+                    val map = HashMap<String, Any>()
+                    map["image"] = url
+                    FirebaseFirestore.getInstance().collection("profileImages").document(uid).set(map)
+                }
+        }
+
     }
 }
--- Howlstagram/app/src/main/java/com/example/howlstagram/navigation/AddPhotoActivity.kt
+++ Howlstagram/app/src/main/java/com/example/howlstagram/navigation/AddPhotoActivity.kt
@@ -1,4 +1,95 @@
 package com.example.howlstagram.navigation
 
-class AddPhotoActivity {
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.howlstagram.R
+import com.example.howlstagram.model.ContentDTO
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_add_photo.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class AddPhotoActivity: AppCompatActivity() {
+
+    var photoUrl: Uri? = null
+    val PICK_IMAGE_FROM_ALBUM = 0
+
+    var storage: FirebaseStorage? = null
+    var firestore: FirebaseFirestore? = null
+    private var auth: FirebaseAuth? = null
+
+    override fun onCreate(saveInstanceState: Bundle?) {
+        super.onCreate(saveInstanceState)
+        setContentView(R.layout.activity_add_photo)
+
+        storage = FirebaseStorage.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
+
+        val photoPickerIntent = Intent(Intent.ACTION_PICK)
+
+        photoPickerIntent.type = "image/*"
+        startActivityForResult(photoPickerIntent, PICK_IMAGE_FROM_ALBUM)
+
+        addphoto_image.setOnClickListener {
+            val photoPickerIntent = Intent(Intent(Intent.ACTION_PICK))
+            photoPickerIntent.type = "image/*"
+            startActivityForResult(photoPickerIntent, PICK_IMAGE_FROM_ALBUM)
+        }
+        addphoto_btn_upload.setOnClickListener {
+            contentUpload()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == PICK_IMAGE_FROM_ALBUM) {
+            if(resultCode == Activity.RESULT_OK) {
+                println(data?.data)
+                photoUrl = data?.data
+                addphoto_image.setImageURI(data?.data)
+            } else {
+                finish()
+            }
+        }
+    }
+
+    fun contentUpload() {
+        progress_bar.visibility = View.VISIBLE
+
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageFileName = "JPEG_" + timeStamp + "_.PNG"
+        val storageRef = storage?.reference?.child("images")?.child(imageFileName)
+        storageRef?.putFile(photoUrl!!)?.addOnSuccessListener { taskSnapshot ->
+            progress_bar.visibility = View.GONE
+
+            Toast.makeText(this, getString(R.string.upload_success),
+            Toast.LENGTH_LONG).show()
+            val uri = taskSnapshot.downloadUrl
+
+            val contentDTO = ContentDTO()
+
+            contentDTO.imageUrl = uri!!>toString()
+            contentDTO.uid = auth?.currentUser?.uid
+            contentDTO.explain = addphoto_edit_explain.text.toString()
+            contentDTO.userId = auth?.currentUser?.email
+            contentDTO.timestamp = System.currentTimeMillis()
+
+            firestore?.collection("images")?.document()?.set(contentDTO)
+
+            setResult(Activity.RESULT_OK)
+            finish()
+        }
+            ?.addOnFailureListener{
+                progress_bar.visibility = View.GONE
+
+                Toast.makeText(this, getString(R.string.upload_fail),Toast.LENGTH_LONG).show()
+            }
+    }
 }

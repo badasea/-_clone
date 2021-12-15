e18862af932617cb9e9f30650169c0f1287e4668
jungbada269 <57929751+jungbada269@users.noreply.github.com>
2021-12-15 19:11:00 +0900

    댓글 기능 추가 (잇슈 있음)

--- bada_stargram/app/src/main/java/com/example/bada_stargram/navigation/CommentActivity.kt
+++ bada_stargram/app/src/main/java/com/example/bada_stargram/navigation/CommentActivity.kt
@@ -1,13 +1,22 @@
 package com.example.bada_stargram.navigation
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.bada_stargram.R
 import com.example.bada_stargram.model.ContentDTO
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_comment.*
+import kotlinx.android.synthetic.main.item_comment.view.*
 
 class CommentActivity : AppCompatActivity() {
     var contentUid: String? = null
@@ -17,8 +26,8 @@ class CommentActivity : AppCompatActivity() {
         setContentView(R.layout.activity_comment)
         contentUid = intent.getStringExtra("contentUid")
 
-        //comment_recyclerview.adapter = CommentRecyclerViewAdapter()
-        //comment_recyclerview.layoutManager = LinearLayoutManager(this)
+        comment_recyclerview.adapter = CommentRecyclerViewAdapter()
+        comment_recyclerview.layoutManager = LinearLayoutManager(this)
 
         comment_btn_send?.setOnClickListener {
             var comment = ContentDTO.Comment()
@@ -32,6 +41,51 @@ class CommentActivity : AppCompatActivity() {
                 .collection("comments").document().set(comment)
 
             comment_edit_message.setText("")
+        }
+    }
+    inner class CommentRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        var comments: ArrayList<ContentDTO.Comment> = arrayListOf()
+
+        init {
+            FirebaseFirestore.getInstance().collection("images").document(contentUid!!).collection("comments").orderBy("timestamp")
+                .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                    comments.clear()
+
+                    if (querySnapshot == null)
+                        return@addSnapshotListener
+
+                    for (snapshot in querySnapshot.documents!!) {
+                        comments.add(snapshot.toObject(ContentDTO.Comment::class.java)!!)
+                    }
+
+                    notifyDataSetChanged()
+
+                }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            var view = LayoutInflater.from(parent.context).inflate(R.layout.item_comment, parent,false)
+            return CustomViewHolder(view)
+        }
+
+        private inner class CustomViewHolder(view: View): RecyclerView.ViewHolder(view)
+
+        override fun getItemCount(): Int {
+            return comments.size
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            var view = holder.itemView
+            view.commentviewitem_textview_comment.text = comments[position].comment
+            view.commentviewitem_textview_profile.text = comments[position].userId
+
+            FirebaseFirestore.getInstance().collection("profileImages").document(comments[position].uid!!).get().addOnCompleteListener { task ->
+
+                if(task.isSuccessful){
+                    var url = task.result!!["image"]
+                    Glide.with(holder.itemView.context).load(url).apply(RequestOptions().circleCrop()).into(view.commentviewitem_imageview_profile)
+                }
+            }
         }
     }
 }
